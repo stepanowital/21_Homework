ca3a9b21313dc110ba15255d57b5d441f9540d1e
stepanowital <stepanowital@gmail.com>
2022-12-26 00:14:24 +0600

    add from shop to store

--- functions.py
+++ functions.py
@@ -47,44 +47,82 @@ def main():
 			sleep(1)
 			continue
 
-		if not store.check_item(request.product):
-			print("Данного товара нет на складе")
-			print()
-			sleep(1)
-			continue
-		if store.check_quantity_limits(request.product) < request.amount:
-			print("Не хватает на складе, попробуйте заказать меньше")
-			print()
-			sleep(1)
-			continue
-		if shop.get_unique_items_count() >= shop.unique_items_count:
-			if not shop.check_item(request.full_request()["product"]):
-				print(f"В данном магазине уже есть {shop.unique_items_count} разных наименований.")
+		if request.departure == "склад" and request.destination == "магазин":
+			if not store.check_item(request.product):
+				print("Данного товара нет на складе")
 				print()
 				sleep(1)
 				continue
-		if shop.get_free_space() < request.amount:
-			print(f"В {shop.name} недостаточно места, попробуйте что-то другое)")
-			print()
+			if store.check_quantity_limits(request.product) < request.amount:
+				print("Не хватает товара на складе, попробуйте заказать меньше")
+				print()
+				sleep(1)
+				continue
+
+			if shop.get_free_space() < request.amount:
+				print(f"В {shop.name}е недостаточно места, попробуйте что-то другое)")
+				print()
+				sleep(1)
+				continue
+			if shop.get_unique_items_count() >= shop.unique_items_count:
+				if not shop.check_item(request.product):
+					print(f"В данном магазине уже есть {shop.unique_items_count} разных наименований.")
+					print()
+					sleep(1)
+					continue
+			print("Данный товар есть на складе")
 			sleep(1)
-			continue
 
-		print("Данный товар есть на складе")
-		sleep(1)
+			store.remove(
+				request.product,
+				request.amount)
+			sleep(1)
 
-		store.remove(
-			request.product,
-			request.amount)
-		sleep(1)
+			print(
+				f'Курьер везет {request.amount} {request.product} со склада в магазин')
+			sleep(1)
 
-		print(
-			f'Курьер везет {request.amount} {request.product} со склада в магазин')
-		sleep(1)
+			shop.add(request.product, request.amount)
+			sleep(1)
 
-		shop.add(request.product, request.amount)
-		sleep(1)
+			store.get_full_report()
+			sleep(1)
+			shop.get_full_report()
+			sleep(1)
 
-		store.get_full_report()
-		sleep(1)
-		shop.get_full_report()
-		sleep(1)
+		if request.departure == "магазин" and request.destination == "склад":
+			if not shop.check_item(request.product):
+				print("Данного товара нет в магазине")
+				print()
+				sleep(1)
+				continue
+			if shop.check_quantity_limits(request.product) < request.amount:
+				print("Не хватает товара в магазине, попробуйте заказать меньше")
+				print()
+				sleep(1)
+				continue
+			if store.get_free_space() < request.amount:
+				print(f"На {store.name}е недостаточно места, попробуйте что-то другое)")
+				print()
+				sleep(1)
+				continue
+
+			print("Данный товар есть в магазине")
+			sleep(1)
+
+			shop.remove(
+				request.product,
+				request.amount)
+			sleep(1)
+
+			print(
+				f'Курьер везет {request.amount} {request.product} из магазина на склад')
+			sleep(1)
+
+			store.add(request.product, request.amount)
+			sleep(1)
+
+			store.get_full_report()
+			sleep(1)
+			shop.get_full_report()
+			sleep(1)
